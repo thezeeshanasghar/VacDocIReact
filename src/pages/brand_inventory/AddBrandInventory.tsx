00bb21348eb44b77483cc34bedd2e32a5d7711d4
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-06-13 12:27:53 +0500

    brandInv brandamount

--- src/pages/brand_inventory/AddBrandInventory.tsx
+++ src/pages/brand_inventory/AddBrandInventory.tsx
@@ -2,13 +2,17 @@ import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonInput, Io
 import React,{useState,useEffect} from 'react';
 import Header from '../../components/header/Header';
 import Toast from '../../components/custom-toast/Toast';
+import { isEmpty, parseInt } from 'lodash';
 type BrandInventoryType = { Id: number; Name: string };
+type VaccineDataType = { Id: number; Name: string };
 const AddBrandInventory: React.FC = () => {
     const [success, setSuccess] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
     const [brandData, setBrandData] = useState<BrandInventoryType[]>([]);
     const [brandName, setBrandName] = useState("");
-    const [brandCount, setBrandCount] = useState("");
+    const [vaccineData, setVaccineData] = useState<VaccineDataType[]>([]);
+    const [vaccineName, setVaccineName]=useState<string>("");
+    const [brandCount, setBrandCount] = useState<string>("");
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -16,9 +20,10 @@ const AddBrandInventory: React.FC = () => {
         //   new Date(dob),
         //   "yyyy-MM-dd'T'HH:mm:ss.SSSX"
         // );
-        console.log("qatat")
+        console.log(brandCount)
+        console.log("count = " , brandCount)
         const data_to_be_sent = {
-            "count": brandCount,
+            "count": +brandCount,
             "brandId": brandName,
             "doctorId": 1
         };
@@ -33,24 +38,33 @@ const AddBrandInventory: React.FC = () => {
           .then((res) => (res.status === 201 ? setSuccess(true) : setError(true)))
           .catch((err) => setError(true))
           .finally(() => {
-            clearStateVariables();
+            // clearStateVariables();
           });
       };
-      const clearStateVariables = () => {
-        setBrandName("");
-        setBrandCount("");
+      // const clearStateVariables = () => {
+      //   setBrandName("");
+      //   setBrandCount("");
         
-      };
-      useEffect(() => {
-        fetch("http://localhost:5041/BrandName")
+      // };
+      const handleBrandCount=()=>{
+       console.log(brandCount)
+      }
+      const handleClickVaccine=()=>{
+        fetch(`http://localhost:5041/api/Brand/brand_name/${vaccineName}`)
           .then((res) => res.json())
           .then((data) => setBrandData(data))
           .catch((err) => console.error(err));
-    
-        // fetch("https://myapi.fernflowers.com/api/Clinic")
-        //   .then((res) => res.json())
-        //   .then((data) => setClinicData(data))
-        //   .catch((err) => console.error(err));
+      }
+      useEffect(() => {
+          fetch("http://localhost:5041/api/Vaccine")
+          .then((res) => res.json())
+          .then((data) => setVaccineData(data))
+          .catch((err) => console.error(err));
+
+          // fetch(`http://localhost:5041/api/Brand/brand_name/1`)
+          // .then((res) => res.json())
+          // .then((data) => setVaccineName(data))
+          // .catch((err) => console.error(err));
       }, []);
 
     return (
@@ -72,7 +86,7 @@ const AddBrandInventory: React.FC = () => {
             <IonContent>
             <IonCard>
         <IonCardContent>
-        
+          <form onSubmit={handleSubmit}>
           <IonItem>
               <IonLabel position="floating">Brand Count</IonLabel>
               <IonInput
@@ -82,10 +96,25 @@ const AddBrandInventory: React.FC = () => {
                 required
               />
             </IonItem>
+        <IonItem>
+            <IonLabel position="floating">Vaccine Name</IonLabel>
+              <IonSelect
+                value={vaccineName}
+                onIonChange={(e) => setVaccineName(e.detail.value!)}
+              >
+                {vaccineData &&
+                  vaccineData.map((item, index) => (
+                    <IonSelectOption key={index} value={item.Id}>
+                      {item.Name}
+                    </IonSelectOption>
+                  ))}
+              </IonSelect>
+            </IonItem>
             <IonItem>
-            <IonLabel position="floating">Brand Name</IonLabel>
+            <IonLabel position="floating">Selected Vaccine Brand Name</IonLabel>
               <IonSelect
                 value={brandName}
+                onClick={handleClickVaccine}
                 onIonChange={(e) => setBrandName(e.detail.value!)}
               >
                 {brandData &&
@@ -96,10 +125,21 @@ const AddBrandInventory: React.FC = () => {
                   ))}
               </IonSelect>
             </IonItem>
-            <IonButton onClick={handleSubmit}>
+            {/* <IonItem>
+              <IonLabel position="floating">Count</IonLabel>
+              <IonInput
+                 type="number"
+                 value={brandCount}
+                 onClick={handleBrandCount}
+                 onIonChange={(e) => setBrandCount(e.detail.value!)}
+              />
+            </IonItem> */}
+           
+        
+            <IonButton type='submit'>
               Add Inventory
             </IonButton>
-         
+         </form>
         </IonCardContent>
       </IonCard>
       </IonContent>
--- src/pages/brand_inventory/BrandInventory.tsx
+++ src/pages/brand_inventory/BrandInventory.tsx
@@ -1,17 +1,76 @@
- import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
- import React from 'react';
-import Header from '../../components/header/Header';
- 
- const BrandInventory: React.FC = () => {
- 
-    return (
-        <IonPage>
-            <Header pageName="Brand Inventory" />
-            <IonContent className="ion-padding">
-                UI goes here...
-            </IonContent>
-        </IonPage>
-    );
- };
- 
- export default BrandInventory;
+import {
+  IonContent,
+  IonPage,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+} from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+type BrandInventoryType = { Count: number; VaccineName: string; Brand: string };
+
+const BrandInventory: React.FC = () => {
+  const [brandData, setBrandData] = useState<BrandInventoryType[]>([]);
+  useEffect(() => {
+  fetchInventoryData();
+  }, []);
+
+  const fetchInventoryData = async () => {
+    try {
+      const response = await fetch("http://localhost:5041/api/BrandInventory/doctor-vaccine-Count/1"); // Replace 'API_ENDPOINT' with the actual API endpoint URL
+      const data = await response.json();
+      setBrandData(data);
+    } catch (error) {
+      console.error('Error fetching brand data:', error);
+    }
+  };
+
+  return (
+    <IonPage>
+      <Header pageName="Brand Inventory" />
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardHeader
+            style={{
+              color: "black",
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gap: "10px",
+            }}
+          >
+            <b>
+              <div>VaccineName</div>
+            </b>
+            <b>
+              {" "}
+              <div>Brand</div>
+            </b>
+            <b>
+              {" "}
+              <div>Count</div>
+            </b>
+          </IonCardHeader>
+        </IonCard>
+        {brandData &&
+          brandData.map((item, index) => (
+            <IonCard>
+              <IonCardContent
+                style={{
+                  color: "black",
+                  display: "grid",
+                  gridTemplateColumns: "1fr 1fr 1fr",
+                  gap: "10px",
+                }}
+              >
+                <div>{item.VaccineName}</div>
+                <div>{item.Brand}</div>
+                <div>{item.Count}</div>
+              </IonCardContent>
+            </IonCard>
+          ))}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default BrandInventory;
