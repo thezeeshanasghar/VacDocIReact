61d1bb35b406c1cabc5faa4280f5b38c728fa279
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-06-15 12:15:12 +0500

    alert commit

--- src/pages/alert/Alert.tsx
+++ src/pages/alert/Alert.tsx
@@ -1,14 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonButton, IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React,{useState,useEffect} from 'react';
 import Header from '../../components/header/Header';
-
+type PatientDataType = { Id: number; Name: string; ClinicId: number;DoctorId: number};
 const Alert: React.FC = () => {
+    const [patientData, setPatientData] = useState<PatientDataType[]>([]);
+    const fetchPatientData = () => {
+        fetch("http://localhost:5041/patients?doctorId=1")
+          .then((response) => response.json())
+          .then((data) => setPatientData(data))
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+    const ClickHandler=(ClinicId:number,DoctorId:number,name:string)=>{
+console.log("ClinicId:",ClinicId,"DoctorId:",DoctorId,"name:",name)
+    }
+      useEffect(() => {
+        fetchPatientData();
+      }, []);
 
     return (
         <IonPage>
             <Header  pageName='Alert'/>
             <IonContent className="ion-padding">
-                Alert content goes here.
+            {patientData &&
+              patientData.map((item, index) => {
+                return(
+                    <div key={item.Id}>
+                    <IonCard onClick={() => ClickHandler(item.ClinicId, item.DoctorId, item.Name)}>
+                    <IonCardHeader>
+                      <IonCardTitle>{item.Name}</IonCardTitle>
+                      {/* <IonCardSubtitle>Card Subtitle</IonCardSubtitle> */}
+                    </IonCardHeader>
+                    </IonCard>
+                    </div>
+                )
+              })}
             </IonContent>
         </IonPage>
     );
