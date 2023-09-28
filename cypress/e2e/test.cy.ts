4bbc4264155a7ee96390595a979685818e69bd5c
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-09-28 12:40:13 +0500

    changes

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -15,24 +15,24 @@ describe('Login Page', () => {
     
     // cy.get("[data-cy=name]").type("John Doe");
 
-// cy.get('#sign').click();
-//     cy.get("#name").type("John Doe");
-//     cy.get("#email").type("john.doe@example.com");
-//     cy.get("#mobileNumber").type("1234567890");
-//     cy.get("#password").type("password123");
-//     cy.get("#pmdc").type("123456789");
-//     cy.get('body').click();
-//     cy.wait(3000);
-//     cy.get("#signup").click();
-//     cy.get("#names").type("hassan clinic");
-//     cy.get("#address").type("b-17 Islamabad");
-//     cy.get("#mobilenumbers").type("123456789");
-//     cy.get('body').click();
-//     cy.get("#submits").click();
-//     cy.get("#test").click();
-//     cy.get("#test1").click();
-//     cy.get("#test2").type("12:00");
-//     cy.get("#test3").type("12:45");
+cy.get('#sign').click();
+    cy.get("#name").type("John Doe");
+    cy.get("#email").type("john.doe@example.com");
+    cy.get("#mobileNumber").type("1234567890");
+    cy.get("#password").type("password123");
+    cy.get("#pmdc").type("123456789");
+    cy.get('body').click();
+    cy.wait(3000);
+    cy.get("#signup").click();
+    cy.get("#names").type("hassan clinic");
+    cy.get("#address").type("b-17 Islamabad");
+    cy.get("#mobilenumbers").type("123456789");
+    cy.get('body').click();
+    cy.get("#submits").click();
+    cy.get("#test").click();
+    cy.get("#test1").click();
+    cy.get("#test2").type("12:00");
+    cy.get("#test3").type("12:45");
 
 
     // Click the Submit button
