6ac52f5714c483728d077125ba2d3350ac788dfb
codewithumarrr <94783559+codewithumarrr@users.noreply.github.com>
2023-11-12 16:11:25 +0500

    global alert

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,44 @@ import Login from "./pages/login/Login";
 import DoctorSignUp from "./pages/doctor-signup/signup/DoctorSignUp";
 import ClinicRegistration from "./pages/doctor-signup/add-clinic/ClinicRegistration";
 import ClinicSchedule from "./pages/doctor-signup/doc_schedule/ClinicSchedule";
+import { useEffect } from "react";
 
 setupIonicReact();
 
 const App: React.FC = () => {
+  useEffect(() => {
+    window.alert = function (message) {
+      var alertContainer = document.createElement("div");
+      alertContainer.className = "custom-alert";
+
+      alertContainer.style.position = "fixed";
+      alertContainer.style.top = "10px";
+      alertContainer.style.right = "10px";
+      alertContainer.style.width = "max-content";
+      alertContainer.style.padding = "14px 20px";
+      alertContainer.style.fontSize = "11px";
+      alertContainer.style.backgroundColor = "#f44336";
+      alertContainer.style.color = "white";
+      alertContainer.style.borderRadius = "5px";
+      alertContainer.style.opacity = "0";
+      alertContainer.style.transition = "opacity 0.5s ease-in-out";
+
+      alertContainer.textContent = message;
+
+      document.body.appendChild(alertContainer);
+
+      // Show the alert
+      setTimeout(function () {
+        alertContainer.style.opacity = "1";
+      }, 100);
+      setTimeout(function () {
+        alertContainer.style.opacity = "0";
+        setTimeout(function () {
+          document.body.removeChild(alertContainer);
+        }, 500);
+      }, 3000);
+    };
+  });
   return (
     <IonApp>
       <IonReactRouter>
